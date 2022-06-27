ea97aa23ca5c0e5da618de56d27b35a85a5ebaa4
Mariana Muniz <101331946+MarianaGSMuniz@users.noreply.github.com>
2022-06-27 19:16:01 -0300

    carregamento de arquivo

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 const chalk = require('chalk');
 const fs = require('fs')
 
-function pegaArquivo(caminhoDoArquivo) {
-    const encoding = 'utf-8'
-    fs.readFile(caminhoDoArquivo, encoding, (_, texto) => {
-        console.log(chalk.green(texto))
+function trataErro(erro) {
+    throw new Error(chalk.red(erro.code, 'não há arquivos'));
 
-    })
 }
-pegaArquivo('./arquivos/texto.md');
+
+async function pegaArquivo(caminhoDoArquivo) {
+    const encoding = 'utf-8';
+    const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+    console.log(chalk.green(texto))
+}
+
+//function pegaArquivo(caminhoDoArquivo) {
+//  const encoding = 'utf-8';
+//fs.promises
+//  .readFile(caminhoDoArquivo, encoding)
+//.then((texto) => console.log(texto))
+//.catch((erro) => trataErro(erro))
+
+
+}
+
+//function pegaArquivo(caminhoDoArquivo) {
+//  const encoding = 'utf-8'
+//fs.readFile(caminhoDoArquivo, encoding, (erro, texto) => {
+//  if (err) {
+//    trataErro(erro);
+//}
+
+//console.log(chalk.green(texto))
+
+// })
+//}
+//pegaArquivo('./arquivos/texto.md');
