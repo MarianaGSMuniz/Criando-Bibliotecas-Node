782940815ea28ee8f4cb886190cd72a3ec370c3a
Mariana Muniz <maewerton60@gmail.com>
2022-07-04 20:49:25 -0300

    atualizando

--- http-valida.js
+++ http-valida.js
@@ -1,11 +1,17 @@
+const fetch= require ('node-fetch');
+
+function checaStatus (arrayLinks){
+  
+}
+
 function geraLink (arrayLinks){
   // objeto ->[valor]
     object.values (objeto);
-
+return arrayLinks.map (objetoLink =>Object.values(objetoLink).join());
 }
 
 function validaURLs(arrayLinks) {
-    return arrayLinks;
+    return geraLink  (arrayLinks);
 }
 
 module.exports = validaURLs;
