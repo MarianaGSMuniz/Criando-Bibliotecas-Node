42db0ae00678e013a036355338d4d7880487ba97
Mariana Muniz <maewerton60@gmail.com>
2022-07-04 20:17:59 -0300

    atualizando

--- http-valida.js
+++ http-valida.js
@@ -1,5 +1,13 @@
+function geraLink (arrayLinks){
+  // objeto ->[valor]
+    object.values (objeto);
+
+}
+
 function validaURLs(arrayLinks) {
     return arrayLinks;
 }
 
 module.exports = validaURLs;
+
+
